cf9c083c6e8f24cc339ed6060c02d1d3ae6ec6fd
ThomasCode92 <thomascode92@hotmail.com>
2024-01-25 07:38:46 +0100

    Add Font to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import "@/styles/globals.css";
+import localFont from 'next/font/local';
+
+import '@/styles/globals.css';
+
+const myFont = localFont({ src: '../fonts/RobotoSlab.ttf' });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={myFont.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
