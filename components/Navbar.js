5c85dfa85d867dbe4cd34c91af41cfb3b8f130b4
ThomasCode92 <thomascode92@hotmail.com>
2024-01-31 11:29:36 +0100

    NavBar Router

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,20 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
 import styles from './Navbar.module.css';
 
 export default function Navbar({ username }) {
+  const router = useRouter();
+
+  const handleNavigate = route => {
+    router.push(route);
+  };
+
+  const routes = [
+    { label: 'Home', path: '/' },
+    { label: 'My List', path: '/browse/my-list' },
+  ];
+
   return (
     <nav className={styles.container}>
       <a className={styles['logo-link']}>
@@ -8,14 +22,23 @@ export default function Navbar({ username }) {
       </a>
 
       <ul className={styles['nav-items']}>
-        <li className={styles['nav-item']}>Home</li>
-        <li className={styles['nav-item']}>My List</li>
+        {routes.map(({ label, path }) => (
+          <li
+            key={label}
+            className={styles['nav-item']}
+            onClick={handleNavigate.bind(this, path)}
+          >
+            {label}
+          </li>
+        ))}
       </ul>
 
       <div className={styles['username-wrapper']}>
         <button className={styles['username-btn']}>{username}</button>
         <div className={styles['username-dropdown']}>
-          <a className={styles['username-link']}>Sign Out</a>
+          <Link className={styles['signout-link']} href="/login">
+            Sign Out
+          </Link>
         </div>
       </div>
     </nav>
