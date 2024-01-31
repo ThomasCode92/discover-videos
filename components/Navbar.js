a4d2594ab778e2f3b8f7ca206a1503a4a2099e1e
ThomasCode92 <thomascode92@hotmail.com>
2024-01-31 11:30:28 +0100

    Navbar Dropdown

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 import styles from './Navbar.module.css';
 
 export default function Navbar({ username }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const router = useRouter();
 
   const handleNavigate = route => {
+    setIsDropdownOpen(false);
     router.push(route);
+  };
+
+  const handleShowDropdown = () => {
+    setIsDropdownOpen(prevState => !prevState);
   };
 
   const routes = [
@@ -34,12 +41,16 @@ export default function Navbar({ username }) {
       </ul>
 
       <div className={styles['username-wrapper']}>
-        <button className={styles['username-btn']}>{username}</button>
-        <div className={styles['username-dropdown']}>
-          <Link className={styles['signout-link']} href="/login">
-            Sign Out
-          </Link>
-        </div>
+        <button className={styles['username-btn']} onClick={handleShowDropdown}>
+          {username}
+        </button>
+        {isDropdownOpen && (
+          <div className={styles['username-dropdown']}>
+            <Link className={styles['signout-link']} href="/login">
+              Sign Out
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
