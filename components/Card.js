0e517a0f2401f98cf8d6b37350b8186fa67f5ae3
ThomasCode92 <thomascode92@hotmail.com>
2024-02-06 19:37:23 +0100

    Card Component Size

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,19 @@
 import Image from 'next/image';
 
+import styles from './Card.module.css';
+
 export default function Card({ imageUrl, size }) {
   return (
-    <div>
-      <Image src={imageUrl} alt="image" width={300} height={300} />
+    <div className={styles.container}>
+      Card
+      <div className={styles[`${size}-card-image`]}>
+        <Image
+          src={imageUrl}
+          className={styles['card-image']}
+          alt="image"
+          fill={true}
+        />
+      </div>
     </div>
   );
 }
