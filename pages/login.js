7494fedf87074fc4f99c9f497114dc21e1f7a00b
ThomasCode92 <thomascode92@hotmail.com>
2024-02-27 19:07:58 +0100

    Sign-in Page Structure

--- pages/login.js
+++ pages/login.js
@@ -6,19 +6,45 @@ import styles from '@/styles/Login.module.css';
 import netflixLogo from '@/public/static/netflix.svg';
 
 export default function Login() {
+  const handleLoginWithEmail = async event => {
+    event.preventDefault();
+
+    console.log('Login with email...');
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Netflix SignIn</title>
       </Head>
 
-      <header>
+      <header className={styles.header}>
         <Link href="/">
           <div className={styles['logo-link']}>
             <Image src={netflixLogo} alt="Netflix logo" fill={true} />
           </div>
         </Link>
       </header>
+
+      <main className={styles.main}>
+        <form className={styles['login-form']}>
+          <h1 className={styles['login-header']}>Sign In</h1>
+
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles['email-input']}
+          />
+
+          <p className={styles['user-message']}></p>
+          <button
+            className={styles['login-btn']}
+            onClick={handleLoginWithEmail}
+          >
+            Sign In
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
