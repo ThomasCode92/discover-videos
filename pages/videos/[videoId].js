51c7199b771e2824492fed4ef73d4f696bc0e503
ThomasCode92 <thomascode92@hotmail.com>
2024-03-11 08:55:26 +0100

    Modal Component Structure

--- pages/videos/[videoId].js
+++ pages/videos/[videoId].js
@@ -1,7 +1,19 @@
 import { useRouter } from 'next/router';
 import Modal from 'react-modal';
+import localFont from 'next/font/local';
+import clsx from 'classnames';
 
 import styles from '@/styles/Video.module.css';
+
+const modalFont = localFont({
+  src: [
+    {
+      path: '../../fonts/RobotoSlab-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
 
 Modal.setAppElement('#__next');
 
@@ -11,6 +23,15 @@ export default function Video() {
   const { videoId } = router.query;
 
   const videoSrc = `http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=0`;
+
+  const video = {
+    title: 'Hi cute dog',
+    publishTime: '1990-01-01',
+    description:
+      'A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?',
+    channelTitle: 'Paramount Pictures',
+    viewCount: 10000,
+  };
 
   return (
     <div className={styles.container}>
@@ -26,10 +47,27 @@ export default function Video() {
           className={styles['video-player']}
           type="text/html"
           width="100%"
-          height="100%"
+          height="360"
           src={videoSrc}
           frameborder="0"
         />
+        <div className={clsx(styles['modal-body'], modalFont.className)}>
+          <div className={styles['video-info']}>
+            <p className={styles['publish-time']}>{video.publishTime}</p>
+            <p className={styles.title}>{video.title}</p>
+            <p className={styles.description}>{video.description}</p>
+          </div>
+          <div className={styles['video-stats']}>
+            <p>
+              <span className={styles['sub-text']}>Cast: </span>
+              {video.channelTitle}
+            </p>
+            <p>
+              <span className={styles['sub-text']}>View Count: </span>
+              {video.viewCount}
+            </p>
+          </div>
+        </div>
       </Modal>
     </div>
   );
