25678a27353a1152a974a523c4f6406b3c8ab2d1
Thomas <thomascode92@hotmail.com>
2024-05-10 23:10:05 +0200

    Parse Auth Header

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,6 +3,9 @@ export default async function login(req, res) {
     return res.status(405).send({ message: 'Method not allowed' });
 
   try {
+    const auth = req.headers.authorization;
+    const token = auth.split(' ')[1];
+    console.log(token);
     return res.send({ message: 'Login successful' });
   } catch (error) {
     console.error(error);
