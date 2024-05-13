c6a20882ab7d22867a038cb22346c298e13d7424
Thomas <thomascode92@hotmail.com>
2024-05-13 07:58:38 +0200

    Magic Server Side SDK - Testing

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,11 +1,16 @@
+import { magicAdmin } from '@/lib/magic-admin';
+
 export default async function login(req, res) {
   if (req.method !== 'POST')
     return res.status(405).send({ message: 'Method not allowed' });
 
   try {
     const auth = req.headers.authorization;
-    const token = auth.split(' ')[1];
-    console.log(token);
+    const didToken = auth.split(' ')[1];
+
+    const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+    console.log('metadata', metadata);
+
     return res.send({ message: 'Login successful' });
   } catch (error) {
     console.error(error);
