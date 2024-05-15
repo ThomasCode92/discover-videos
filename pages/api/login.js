ac061b7e665463f847ee597194b53826815e086c
Thomas <thomascode92@hotmail.com>
2024-05-15 07:58:26 +0200

    Create JWT (JSON Web Token)

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 import { magicAdmin } from '@/lib/magic-admin';
 
 export default async function login(req, res) {
@@ -8,8 +10,25 @@ export default async function login(req, res) {
     const auth = req.headers.authorization;
     const didToken = auth.split(' ')[1];
 
+    // invoke magic
     const metadata = await magicAdmin.users.getMetadataByToken(didToken);
     console.log('metadata', metadata);
+
+    // create jwt
+    const nowInSeconds = Math.floor(Date.now() / 1000);
+    const token = jwt.sign(
+      {
+        iat: nowInSeconds,
+        exp: nowInSeconds + 60 * 60 * 24 * 7, // 1 week
+        'https://hasura.io/jwt/claims': {
+          'x-hasura-allowed-roles': ['user', 'admin'],
+          'x-hasura-default-role': 'user',
+          'x-hasura-user-id': metadata.issuer,
+        },
+      },
+      process.env.JWT_PRIVATE_KEY
+    );
+    console.log('token', token);
 
     return res.send({ message: 'Login successful' });
   } catch (error) {
