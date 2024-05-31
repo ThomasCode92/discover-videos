30e26d55dac488a3ca35da7ec5e1d2b64f5a8a63
Thomas <thomascode92@hotmail.com>
2024-05-31 22:16:08 +0200

    Build Stats with Request Cookies

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -2,5 +2,13 @@ export default async function stats(req, res) {
   if (req.method !== 'POST')
     return res.status(405).send({ message: 'Method not allowed' });
 
-  return res.send({ message: 'Stats updated' });
+  try {
+    if (!req.cookies.token)
+      return res.status(401).send({ message: 'Unauthorized' });
+
+    return res.send({ message: 'Stats updated' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ message: 'Something went wrong' });
+  }
 }
