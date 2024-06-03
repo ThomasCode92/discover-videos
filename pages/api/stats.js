657893bad3600c5fc71bcdf5722e89ee7ee99721
Thomas <thomascode92@hotmail.com>
2024-06-03 20:06:57 +0200

    Tweak Stats API to GET Stats

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -7,15 +7,12 @@ import {
 } from '@/lib/hasura';
 
 export default async function stats(req, res) {
-  if (req.method !== 'POST')
+  if (req.method !== 'POST' && req.method !== 'GET')
     return res.status(405).send({ message: 'Method not allowed' });
 
   try {
     const token = req.cookies.token;
     const videoId = req.query.videoId;
-    const { watched, favoured } = req.body;
-
-    const data = { watched, favoured };
 
     if (!token) return res.status(401).send({ message: 'Unauthorized' });
 
@@ -23,6 +20,14 @@ export default async function stats(req, res) {
     const userId = decodedToken.issuer;
 
     const stats = await findStatsByUserAndVideoId(token, userId, videoId);
+
+    if (req.method === 'GET') {
+      if (!stats) return res.status(404).send({ message: 'Stats not found' });
+      return res.send({ message: 'Stats found', data: stats });
+    }
+
+    const { watched, favoured } = req.body;
+    const data = { watched, favoured };
 
     if (stats) {
       const updatedStats = await updateStatsForUserId(
