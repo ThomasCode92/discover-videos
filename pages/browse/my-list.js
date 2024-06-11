7b35105bfa9abb0ef8bab940b69211c4625b405a
Thomas <thomascode92@hotmail.com>
2024-06-11 19:17:49 +0200

    Server Side Render the "My List" Page

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -4,9 +4,12 @@ import { Fragment } from 'react';
 import Navbar from '@/components/Navbar';
 import SectionCards from '@/components/SectionCards';
 
+import { getFavouredVideos } from '@/lib/videos';
+import verifyToken from '@/utils/verify-token';
+
 import styles from '@/styles/MyList.module.css';
 
-export default function MyList() {
+export default function MyList({ favouredVideos }) {
   return (
     <Fragment>
       <Head>
@@ -17,10 +20,22 @@ export default function MyList() {
         <SectionCards
           className={styles['section-cards']}
           title="My list"
-          videos={[]}
+          videos={favouredVideos}
           size="small"
         />
       </main>
     </Fragment>
   );
+}
+
+export async function getServerSideProps(context) {
+  const token = context.req.cookies?.token ?? null;
+  const userId = verifyToken(token);
+
+  if (!token || !userId)
+    return { redirect: { destination: '/login', permanent: false } };
+
+  const favouredVideos = await getFavouredVideos(token, userId);
+
+  return { props: { favouredVideos } };
 }
